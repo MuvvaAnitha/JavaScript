44d85303e158b92d3174ea8d5582bbd595ef6f3c
MuvvaAnitha <103639428+MuvvaAnitha@users.noreply.github.com>
2022-11-17 12:41:00 +0530

    Add files via upload

--- Functions.js
+++ Functions.js
@@ -1,5 +1,13 @@
 //block of code can be executed together by wrapping them in a module called function.
 
+// Why Functions?
+// You can reuse code: Define the code once, and use it many times.
+
+// You can use the same code many times with different arguments, to produce different results.
+
+
+
+
 function add(a,b)
 {
     return a+b
@@ -18,4 +26,25 @@ let sumofIntegers= function(c,d)
 
 let sumofnumbers=(c,d)=>c+d
 console.log(sumofnumbers(2,3))
+
+//Arrow Function: It is one of the most used and efficient methods to create a function in JavaScript
+let function_name = (argument1, argument2 ,) => expression
+
+//Function Parameters
+
+	// Function definition
+	function welcomeMsg(name) {
+	console.log("Hello " + name +" welcome to javaScriptprograms");
+	}
+	
+	// creating a variable
+	var nameVal = "Admin";
+	
+	// calling the function
+	welcomeMsg(nameVal);
+
+    //Window print() Method
+
+
+
 
